716ca809e80ad1d67b36bbf9e17f403e75572d60
fiqryq <fiqrychoerudin@student.telkomuniversity.ac.id>
2021-12-02 10:19:45 +0700

    init ?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pb-10">
+      <div className="container mx-auto px-3">
+        <div className="w-full h-screen space-y-2 flex flex-col justify-evenly">
+          <div className="flex justify-between">
+            <h1 className="font-bold font-mono text-xl">Wall of Resolution</h1>
+          </div>
+
+          <div className="w-full">
+            <h1 className="font-mono font-bold text-6xl pt-10">
+              Siapa yang cita citanya pengen kaya?
+            </h1>
+          </div>
+
+          <div className="flex justify-between pt-10">
+            <div className="flex flex-col">
+              <h1 className="text-3xl font-bold font-mono">Fiqry choerudin</h1>
+              <div className="flex space-x-10 pt-5">
+                <p className="font-mono max-w-md">
+                  Frontend enginer - based in bandung
+                </p>
+                <p className="font-mono font-bold max-w-xl">
+                  Fiqry choerudin. Saya adalah seorang Front-end Developer
+                  berusia 22 tahun yang sekarang tinggal di Bandung. Saat ini,
+                  saya telah bekerja di Pusat Teknogi Informasi di salah satu
+                  kampus di Kota Bandung selama 8 bulan.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-between">
+              <img
+                alt="profile"
+                src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                width="200"
+                height="100"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="space-y-10 w-full h-screen">
+          <h1 className="font-bold font-mono text-3xl text-left mx-11">
+            All Resolution
+          </h1>
+          <div className="grid grid-cols-3 gap-4">
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+            {/* card */}
+            <div className="w-80 h-72 bg-gray-100 p-8 mx-auto flex flex-col justify-between rounded-md">
+              <p className="font-mono pt-5">
+                Fiqry choerudin. Saya adalah seorang Front-end Developer berusia
+                22 tahun yang sekarang tinggal di Bandung.
+              </p>
+              <div className="flex space-x-2">
+                <img
+                  className="rounded-full h-5 w-5"
+                  alt="profile"
+                  src="https://images.unsplash.com/photo-1638043883373-bb3f45f23033?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                />
+                <p className="font-mono">Fiqry choerudin</p>
+              </div>
+            </div>
+            {/* end card */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
